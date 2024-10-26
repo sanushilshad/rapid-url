271694b63b2b514a89e37e158d8ecb9a943295e5
Sanu Shilshad <57071385+sanushilshad@users.noreply.github.com>
2024-10-26 14:22:29 +0530

    fix: Update release.sh

--- release.sh
+++ release.sh
@@ -2,6 +2,48 @@
 source env.sh
 executable_name="$APPLICATION__NAME" 
 directory="./target/release"
+
+
+# run the test cases
+if cargo test; then
+    echo "Tests passed."
+else
+    echo "Tests failed." >&2
+    exit 1
+fi
+
+# clean the dev instance
+if cargo clean; then
+    echo "Clean successful."
+else
+    echo "Clean failed." >&2
+    exit 1
+fi
+
+## create the binary
+if cargo build --bin "$executable_name" --release; then
+    echo "Build successful."
+else
+    echo "Build failed." >&2
+    exit 1
+fi
+
+
+
+# Clean up the target directory apart from binary
+if [ -d "$directory" ]; then
+    find "$directory" -mindepth 1 ! -name "$executable_name" -exec rm -rf {} +
+    echo "Successfully cleaned up the build directory."
+else
+    echo "Directory $directory does not exist." >&2
+    exit 1
+fi
+
+
+## Running the migrations
+"$directory/$executable_name" -- migrate
+
+## kill the running ports
 PIDS=($(ps aux | grep "$APPLICATION__ACCOUNT_NAME" | grep "$APPLICATION__NAME" | awk '{print $2}'))
 
 if [ ${#PIDS[@]} -gt 0 ];then
@@ -14,39 +56,6 @@ if [ ${#PIDS[@]} -gt 0 ];then
         done
 fi
 
-# run the test cases
-if cargo test; then
-    echo "Tests passed."
-else
-    echo "Tests failed." >&2
-    exit 1
-fi
-
-# clean
-if cargo clean; then
-    echo "Clean successful."
-else
-    echo "Clean failed." >&2
-    exit 1
-fi
-
-if cargo build --bin "$executable_name" --release; then
-    echo "Build successful."
-else
-    echo "Build failed." >&2
-    exit 1
-fi
-
-# Clean up the target directory
-if [ -d "$directory" ]; then
-    find "$directory" -mindepth 1 ! -name "$executable_name" -exec rm -rf {} +
-    echo "Successfully cleaned up the build directory."
-else
-    echo "Directory $directory does not exist." >&2
-    exit 1
-fi
-
-echo "Successfully executed build script."
-"$directory/$executable_name" -- migrate
+## Start the server
 nohup "$directory/$executable_name" &
-
+echo "Successfully executed build script."
